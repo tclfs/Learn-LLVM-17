8f51f3c6afe630077ec5a6cedfd6b8c24ad349f1
tangcl <tangcl@88.com>
2026-03-07 14:38:13 +0800

    增加头文件，解决编译问题

--- Chapter02/calc/src/CodeGen.cpp
+++ Chapter02/calc/src/CodeGen.cpp
@@ -2,6 +2,7 @@
 #include "llvm/ADT/StringMap.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
